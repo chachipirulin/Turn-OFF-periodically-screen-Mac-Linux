e46ceb3fe03396ef43043539089f33024db0b4d3
chachipirulin <62192287+chachipirulin@users.noreply.github.com>
2021-02-03 10:39:42 +0100

    Add files via upload

---
+++ turnOff.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+
+# osascript -e 'tell application "System Events" to sleep' #HACE QUE TODO EL SISTEMA SE VAYA A REPOSO Y SALE DE LA APP EN LA QUE ESTES
+
+pmset displaysleepnow # solo apaga la pantalla
+
+
+# Script created by the tool UsiScript (http://www.usiscript.com/)
+
